e9079bc812da124b11a13250551eb27a301c2f4a
Robert George <rgeorge1982@gmail.com>
2021-03-31 19:29:07 -0400

    track component, annotation component, commentform component changes

--- frontend/src/components/TracksList/TrackDetail.js
+++ frontend/src/components/TracksList/TrackDetail.js
@@ -1,40 +1,46 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-let lines= {};
-let annotation= {};
-let comment = {};
-
-
-const displayComments = event => {
-    return null;
-}
-const displayAnnotation = event => {
-    let lineId = event.currentTarget.dataset.line_id;
-    let clickedLine = lines[lineId]
-    let clickedAnnotation = clickedLine.Annotation;
-    if (!clickedAnnotation) return null; //eventually returns annotation form 
-    console.log(clickedAnnotation)
-    annotation[clickedAnnotation.id] = clickedAnnotation;
-    return (
-    <div key={clickedAnnotation.id} data-annotation_id={clickedAnnotation.id} className='annotation'>{clickedAnnotation.body}</div>
-    )
-}
-const displayLines = track => {
-    track.Lines.forEach(line => {
-        lines[line.id] = line;
-    })
-    console.log(lines)
-    return(
-        <div className='lyrics'>
-         {track.Lines.map( line => <p key={line.id} data-line_id={line.id} onClick={displayAnnotation} className='line'>{line.linetext}</p>)}
-        </div>
-    )
-}
+import './Tracks.css';
+// import { useDispatch } from 'react-redux';
+// import { useParams } from 'react-router-dom';
 function TrackDetail ({ track }) {
     const [show, setShow] = useState(false);
+    const [annotationVisibility, setAnnotationVisibility] = useState(false)
     const clickShow = () => {setShow(!show)};
-
+    const clickAnnotation = () => (setAnnotationVisibility(!annotationVisibility));
+    let lines= {};
+    let annotation= {};
+    // let comment = {};
+    
+    const displayComments = event => {
+        return null;
+    }
+    const displayAnnotation = event => {
+        let lineId = event.currentTarget.dataset.line_id;
+        let clickedLine = lines[lineId]
+        let clickedAnnotation = clickedLine.Annotation;
+        if (!clickedAnnotation) return null; //eventually returns annotation form or login prompt
+        console.log(clickedAnnotation)
+        annotation[clickedAnnotation.id] = clickedAnnotation;
+        return (<div key={clickedAnnotation.id} data-annotation_id={clickedAnnotation.id} className='annotation'>{clickedAnnotation.body}</div>)
+        
+    }
+    const displayLines = track => {
+        track.Lines.forEach(line => {
+            lines[line.id] = line;
+        })
+        console.log(lines)
+        return(
+            <div className='lyrics'>
+             {
+             track.Lines.map( line => {
+                return (
+                    <p key={line.id} data-line_id={line.id} onClick={clickAnnotation} className='line'>{line.linetext}</p>
+                )}
+            )}
+            </div>
+        )
+                
+    }
     return (
         <div className='track'>
             <h2 className='title' onClick={clickShow}>{track.title}</h2>
