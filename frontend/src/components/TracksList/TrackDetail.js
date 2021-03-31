cdc7b39ba11d7f182feb122db5cd57fbebf24530
Robert George <rgeorge1982@gmail.com>
2021-03-31 10:51:49 -0400

    display lines

--- frontend/src/components/TracksList/TrackDetail.js
+++ frontend/src/components/TracksList/TrackDetail.js
@@ -2,28 +2,45 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOneTrack } from '../../store/track';
-
+let lines= {};
+let annotation;
+const makeLines = track => {
+    track.Lines.forEach(line => {
+        lines[line.id] = line;
+    })
+    console.log(lines)
+    return track.Lines.map( line => <p key={line.id} className='line'>{line.linetext}</p>)
+}
+const makeAnnotations = line => {
+    return line.Annotation.map( annotation => <div key={annotation.id} className='annotation'>{annotation.test}</div>)
+}
 function TrackDetail ({ track }) {
     const [show, setShow] = useState(false);
     const trackId = useParams();
     const dispatch = useDispatch();
+    const clickShow = () => {setShow(!show)};
+
     // const track = useSelector((state)=>{
     //     return state.track[trackId];
     // })
     console.log(track)
-    useEffect(()=>{
-        dispatch((getOneTrack(trackId)));
-    }, [dispatch, trackId])
+    // useEffect(()=>{
+    //     dispatch((getOneTrack(trackId)));
+    // }, [dispatch, trackId])
    
     return (
         <div className='track'>
-            <h2 className='title' onClick={()=>setShow(!show)}>{track.title}</h2>
+            <h2 className='title' onClick={clickShow}>{track.title}</h2>
             <p className='artist'>{track.artist}</p>
             {
                 show?
-                <p className= 'album'>{track.album}</p> : 
-                null
+                <p className= 'album'>album: {track.album}</p> : null  
             }
+            {
+                show?
+                makeLines(track) : null
+            }
+
         </div>
     )
 };
