f24f432c6da8fa6dd042063ad6022e6757db40f8
Robert George <rgeorge1982@gmail.com>
2021-06-01 18:20:18 -0400

    working on a regex

--- frontend/seedGenerator.js
+++ frontend/seedGenerator.js
@@ -1,5 +1,9 @@
-
-
-Array.map(ele =>{
-
-})
+let lyrics = document.querySelectorAll('span.ReferentFragment__Highlight-oqvzi6-1.eZruqd')
+lyrics = Array.from(lyrics)
+lyrics = lyrics.map(ele=> ele.outerText)
+newLyrics = []
+for (let i = 0; i < lyrics.length; i++){
+    if(lyrics[i].includes(/\r?\n/)){
+        lyrics.split(/\r?\n/)
+    }
+}
