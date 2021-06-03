997be79577db23ac7b53ad429e58ec7c2d0a1349
Robert George <rgeorge1982@gmail.com>
2021-06-03 11:40:31 -0400

    updates

--- frontend/seedGenerator.js
+++ frontend/seedGenerator.js
@@ -3,7 +3,22 @@ lyrics = Array.from(lyrics)
 lyrics = lyrics.map(ele=> ele.outerText)
 newLyrics = []
 for (let i = 0; i < lyrics.length; i++){
-    if(lyrics[i].includes(/\r?\n/)){
-        lyrics.split(/\r?\n/)
+    if(lyrics[i].includes('\n')){
+        console.log()
+        let fragments = lyrics[i].split('\n')
+        fragments.forEach(element => {
+            newLyrics.push(element)
+        });
+    } else {
+        newLyrics.push(lyrics[i])
     }
 }
+let lyricsString = ''
+// newLyrics = newLyrics.map((ele, index) => {
+//     return {ordernum: index, linetext: ele, trackId: 1, createdAt: new Date(), updatedAt: new Date() }
+// })
+newLyrics.forEach((ele, index) => {
+    lyricsString = lyricsString + `{ordernum: ${index}, linetext:"${ele}", trackId: 1, createdAt: "${new Date()}", updatedAt:"${new Date()}"},`
+})
+
+console.log(lyricsString)
