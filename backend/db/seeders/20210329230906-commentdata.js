1c1414941bd15e38e70f312c1cce1ae6c307c8ba
Robert George <rgeorge1982@gmail.com>
2021-06-21 16:50:15 -0400

    adding someseeds

--- backend/db/seeders/20210329230906-commentdata.js
+++ backend/db/seeders/20210329230906-commentdata.js
@@ -19,7 +19,64 @@ module.exports = {
           trackId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
-        }
+        },
+        {
+          body: 'I remember this song from the movie "Over the Hedge"',
+          userId: 2,
+          trackId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'That is super lame.',
+          userId: 3,
+          trackId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'First.',
+          userId: 3,
+          trackId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'First.',
+          userId: 3,
+          trackId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'Generating comments is harder than you think',
+          userId: 1,
+          trackId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'This is not a faker comment, it is bespoke and hand crafted.',
+          userId: 2,
+          trackId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'This is a bop.',
+          userId: 2,
+          trackId: 6,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          body: 'Really?',
+          userId: 3,
+          trackId: 6,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        
     ], {});
   },
 
